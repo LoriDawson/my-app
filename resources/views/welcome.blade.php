e63b571327803d835469277cd0fcd0439101651f
Lori Dawson <loridawson@gmail.com>
2020-02-22 17:02:07 -0500

    Update home page for new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome To Laravel 6
                 </div>
 
                 <div class="links">
